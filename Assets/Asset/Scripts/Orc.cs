791329ed26034194a44088445d863090ded0acbe
Pouya Pournasir <pouya__1__pournasir@hotmail.com>
2018-09-25 13:26:26 +0330

    My Project with some codes

---
+++ Assets/Asset/Scripts/Orc.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orc : Enemy {
+
+	// Use this for initialization
+ 	public override void Yell()
+	{
+		base.Yell ();
+		Debug.Log ("Orc version of the Yell() method");
+	}
+
+}
---
+++ Assets/Asset/Scripts/Warband.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warband : MonoBehaviour {
+
+	void Start () 
+	{
+		Humanoid human = new Humanoid();
+		Humanoid enemy = new Enemy();
+		Humanoid orc = new Orc();
+
+
+		//Notice how each Humanoid variable contains
+		//a reference to a different class in the
+		//inheritance hierarchy, yet each of them
+		//calls the Humanoid Yell() method.
+		human.Yell();
+		enemy.Yell();
+		orc.Yell();
+
+
+	}
+}
